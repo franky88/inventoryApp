991ee5b929d3cc680cf8bd53cc7ced01a3216fe8
Frank Ramos <ramosfp99@gmail.com>
2019-03-30 16:04:58 +0800

    user updates

--- inventory/__init__.py
+++ inventory/__init__.py
@@ -9,5 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventory.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from inventory import routes
--- inventory/routes.py
+++ inventory/routes.py
@@ -1,8 +1,10 @@
-from flask import render_template, flash, url_for, redirect
-from inventory.forms import RegistrationForm, LoginForm
+import os
+import secrets
+from flask import render_template, flash, url_for, redirect, request
+from inventory.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from inventory.models import User, Post
 from inventory import app, bcrypt, db
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -46,7 +48,11 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            if next_page:
+                return redirect(next_page)
+            else:
+                return redirect(url_for('home'))
         else:
             flash("Login unsuccessful. Please check email and password.", "danger")
     return render_template('inventory/login.html', title="login", form=form)
@@ -56,8 +62,29 @@ def logout():
     logout_user()
     return redirect(url_for("home"))
 
-@app.route('/account')
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile picture', picture_fn)
+    form_picture.save(picture_path)
+    return picture_fn
+
+@app.route('/account', methods=["GET", "POST"])
+@login_required
 def account():
-    if not current_user.is_authenticated:
-        return redirect(url_for('login'))
-    return render_template('inventory/account.html', title="account")
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your account has been updated!", "success")
+        return redirect(url_for('account'))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile picture/' + current_user.image_file)
+    return render_template('inventory/account.html', title="account", image_file=image_file, form=form)
