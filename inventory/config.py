96925e248e5d74aeab8eee092b8c6e2210e22038
Franklin P. Ramos <franky88@users.noreply.github.com>
2019-05-15 14:57:30 +0800

    Update config.py

--- inventory/config.py
+++ inventory/config.py
@@ -8,5 +8,5 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_ADD') #'hubecomputer@gmail.com'# os.environ.get('EMAIL_ADD')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS') #'Hub3T3ch'# os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = os.environ.get('EMAIL_ADD')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
